3d8759a409642cf88e11bea1c5a149ee561c8320
swajp <stejskalmira@icloud.com>
2023-10-09 09:49:08 +0200

    opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,28 @@ export const metadata: Metadata = {
   title: "SHEAN - Narozeniny",
   description:
     "Slavíme 15. let! Vyzvedněte si od nás dárek, který vám vykouzlí úsměv na tváři. Po zadání emailu se vám ozveme a domluvíme se společně, jak váš dárek budete chtít využít.",
+  openGraph: {
+    title: "SHEAN - Narozeniny",
+    description:
+      "Slavíme 15. let! Vyzvedněte si od nás dárek, který vám vykouzlí úsměv na tváři. Po zadání emailu se vám ozveme a domluvíme se společně, jak váš dárek budete chtít využít.",
+    url: "https://shean-zadani.vercel.app/",
+    siteName: "SHEAN - Narozeniny",
+    images: [
+      {
+        url: "/example.jpg",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "/example.jpg",
+        width: 1800,
+        height: 1600,
+        alt: "SHEAN - Narozeniny. Slavíme 15. let! Vyzvedněte si od nás dárek, který vám vykouzlí úsměv na tváři. Po zadání emailu se vám ozveme a domluvíme se společně, jak váš dárek budete chtít využít.",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
